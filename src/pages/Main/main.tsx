00115bf38ec6d2ddc5077511895a2d82f7f8c06e
FremyXS <70861067+FremyXS@users.noreply.github.com>
2022-12-17 17:11:11 +0500

    feat: add filtres

--- src/pages/Main/main.tsx
+++ src/pages/Main/main.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react'
-import { Button, ButtonGroup } from '@vkontakte/vkui';
+import { Button, ButtonGroup, Checkbox, FormItem, FormLayout, IconButton, ModalPage, ModalPageHeader, ModalRoot, PanelHeader, PanelHeaderButton, PanelHeaderClose, SubnavigationButton } from '@vkontakte/vkui';
 import { Avatar, Card, CardGrid, Div, Header, InfoRow, Panel, SimpleCell, SplitCol, SplitLayout, Title, View } from '@vkontakte/vkui';
 import { UserType,  UserInfo} from '@Pages/Main/types';
 import bridge from '@vkontakte/vk-bridge';
-import { Icon12Cancel, Icon12Check } from '@vkontakte/icons';
+import { Icon12Cancel, Icon12Check, Icon20FunnelOutline, Icon24Dismiss } from '@vkontakte/icons';
 // 186800902
 
 enum SexSwitch {
@@ -11,6 +11,18 @@ enum SexSwitch {
     'Женский' = 1,
     'Мужской' = 2,
 }
+
+const FILTERS_USER_SEX = [
+  { value: 0, label: 'Не указан' },
+  { value: 1, label: 'Женский' },
+  { value: 2, label: 'Мужской' },
+]
+
+const FILTERS_DOG_SEX = [
+    { value: 0, label: 'Не указан' },
+    { value: 1, label: 'СУКА' },
+    { value: 2, label: 'КАБЕЛЬ' },
+]
 
 const Main = () => {
     const[searchUser, setSearchUser] = React.useState<UserType>({
@@ -47,6 +59,10 @@ const Main = () => {
         getUserAsync();
     }, [])
 
+    const [filtersModalOpened, setFiltersModalOpened] = React.useState(false);
+    const[filtersUserSex, setFiltersUserSex] = React.useState([0]);
+    const[filtersDogSex, setFiltersDogSex] = React.useState([0]);
+
     function onAppenedClick(){
 
     }
@@ -55,13 +71,117 @@ const Main = () => {
 
     }
 
+    const openModal = () =>{
+        setFiltersModalOpened(true);
+    }
+
+    const closeModal = () =>{
+        setFiltersModalOpened(false);
+    }
+
+    const onChangeFilterUserSex = (e) => {
+        const { value, checked } = e.currentTarget;
+        if (checked) {
+            setFiltersUserSex([...filtersUserSex, +value]);
+        } else {
+            setFiltersUserSex(filtersUserSex.filter((v) => v !== +value));
+        }
+    };
+
+    const onChangeFilterDogSex = (e) => {
+        const { value, checked } = e.currentTarget;
+        if (checked) {
+            setFiltersDogSex([...filtersDogSex, +value]);
+        } else {
+            setFiltersDogSex(filtersDogSex.filter((v) => v !== +value));
+        }
+    };
+
+    const applyFilters = () => {    
+        closeModal();
+    };
+
+    const MODAL_NAME = 'filters'
+
+    const modal = (
+        <ModalRoot
+          activeModal={filtersModalOpened ? MODAL_NAME : null}
+          onClose={closeModal}
+        >
+          <ModalPage 
+            id={MODAL_NAME}
+            header={
+              <ModalPageHeader
+                before={
+                    <PanelHeaderClose onClick={closeModal} />
+                }
+                after={
+                    <PanelHeaderButton onClick={closeModal}>
+                        <Icon24Dismiss />
+                    </PanelHeaderButton>
+                }
+              >
+                Фильтры
+              </ModalPageHeader>
+            }
+          >
+            <FormLayout>
+            <Header>Хозяин</Header>
+              <FormItem top="Пол">
+                {FILTERS_USER_SEX.map(({ value, label }) => {
+                  return (
+                    <Checkbox
+                      value={value}
+                      checked={filtersUserSex.includes(value)}
+                      onChange={onChangeFilterUserSex}
+                    >
+                      {label}
+                    </Checkbox>
+                  );
+                })}
+              </FormItem>
+              <Header>Собака</Header>
+              <FormItem top="Пол">
+                {FILTERS_DOG_SEX.map(({ value, label }) => {
+                  return (
+                    <Checkbox
+                      value={value}
+                      checked={filtersDogSex.includes(value)}
+                      onChange={onChangeFilterDogSex}
+                    >
+                      {label}
+                    </Checkbox>
+                  );
+                })}
+              </FormItem>
+              <FormItem>
+                <Button size="l" stretched onClick={applyFilters}>
+                  Показать результаты
+                </Button>
+              </FormItem>
+            </FormLayout>
+          </ModalPage>
+        </ModalRoot>
+      );
+
     return(
-        <View activePanel='panel1'>
+        <SplitLayout modal={modal}>
+            <SplitCol>
+            <View activePanel='panel1'>
             <Panel id="panel1">
+                <PanelHeader>
+                    <Div style={{display: 'flex', justifyContent: 'space-between'}}>
+                        Поиск
+                        <SubnavigationButton
+                        before={<Icon20FunnelOutline />}
+                        onClick={openModal}
+                        expandable />
+                    </Div>
+                </PanelHeader>
                 <CardGrid size={'l'}>
                     <Card mode="shadow">
                         <Div style={{display: 'flex', justifyContent:'center', alignItems: 'center'}}>
-                            <Avatar size={225}>                            
+                            <Avatar size={225} src={userInfo.photo_max_orig}>                            
                             </Avatar>
                         </Div>
                     </Card>
@@ -119,6 +239,9 @@ const Main = () => {
                 </CardGrid>
             </Panel>
         </View> 
+            </SplitCol>
+        </SplitLayout>
+        
     )
 
     async function getUserAsync() {
